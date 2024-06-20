33cd18598e32d5e0e5e2e46839c7b244a5fbd418
radriann21 <adrianrangelargu@gmail.com>
2024-06-19 23:44:12 -0400

    finished project

--- apiService.php
+++ apiService.php
@@ -1,3 +1,17 @@
 <?php
-  // servicio de la api
+  const API_URL = "https://whenisthenextmcufilm.com/api";
+
+// servicio de la api
+  function apiCall() {
+    $ch = curl_init(API_URL);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    if(curl_errno($ch)) {
+      echo 'Error en cURL ' . curl_error($ch);
+    }
+    curl_close($ch);
+
+    return json_decode($response, true);
+  }
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,29 @@
+<?php 
+  require_once 'apiService.php';
+  $data = apiCall();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/pico.classless.min.css" />
+  <link rel="stylesheet" href="styles.css">
   <title>PHP Practice</title>
 </head>
 <body>
-  <h1>Hello PHP</h1>
+  <main>
+    <h1>¿What movie is the next?</h1>
+    <img src="<?= $data["poster_url"] ?>" alt="<?= $data["title"] ?>">
+    <section>
+      <h2><?= $data["title"] ?></h2>
+      <span><?= "<strong>Days until</strong>: " . $data["days_until"] ?></span>
+      <p><?= "<strong>Overview</strong>: " . $data["overview"] ?></p>
+      <p>
+        <strong>Next movie:</strong>
+        <?= $data["following_production"]["title"] ?>
+      </p>
+    </section>
+  </main>
 </body>
 </html>
