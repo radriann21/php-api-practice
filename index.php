51209f4438ff39377a6f29cb3b41e272dfe3ee14
radriann21 <adrianrangelargu@gmail.com>
2024-06-19 23:52:10 -0400

    changed page title

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="styles.css">
-  <title>PHP Practice</title>
+  <title>Next Movie in UCM</title>
 </head>
 <body>
   <main>
